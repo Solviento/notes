cf7797580eb6060f52c86e84aa764ee8c7832bef
solviento <jp3476@columbia.edu>
2019-02-12 20:49:36 -0500

    ..

---
+++ leetcode/HouseRobber.java
@@ -0,0 +1,20 @@
+// Y are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+// Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+// Input: [1,2,3,1]
+// Output: 4
+// Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+//              Total amount you can rob = 1 + 3 = 4
+class HouseRobber {
+    public int rob(int[] nums) {
+        // [3][2][1][5][6]
+        if (nums.length == 0) return 0;
+        int prev1 = 0;
+        int prev2 = 0;
+        for (int num : nums) {
+            int tmp = prev1;
+            prev1 = Math.max(prev2 + num, prev1);
+            prev2 = tmp;
+        }
+        return prev1;
+    }
+}
---
+++ leetcode/Number1Bits.java
@@ -0,0 +1,17 @@
+// Write a function that takes an unsigned integer and return the number of '1' bits it has (also known as the Hamming weight).
+// an integer in java has 32 bits
+// to count 1's in the bit representation of integer n, we do n AND 1, if the operation result is 1 then that means the last bit of the input integer is 1. We then add this to the 1's count
+// shift the integer n by on to the right and check for the next bit
+// use >>> bit shifting unsigned operation due to Integer.Max_val +1 = Integer.Min_val which forces us to not use n > 0 and use n!=0 instead
+
+public class Number1Bits {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int ones = 0;
+        while(n!=0){
+            ones = ones + (n&1);
+            n = n>>>1;
+        }
+        return ones;
+    }
+}
