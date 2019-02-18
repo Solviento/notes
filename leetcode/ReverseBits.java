9c8bbd7948a3f8011cd9d60b30f355e7b8fc802f
solviento <jp3476@columbia.edu>
2019-02-17 22:02:20 -0500

    ..

--- leetcode/ReverseBits.java
+++ leetcode/ReverseBits.java
@@ -1,7 +1,19 @@
+// Reverse bits of a given 32 bits unsigned integer.
+// the idea, an int is a 32 long bit
+// so to transform 0000 ... 1000 to 0001 ... 0000
+// We traverse the bit representation (32 iterations)
 class ReverseBits{
     public int reverseBits(int x){
-
-        return x;
+        int reversed = 0;
+        int position = 32-1;    // used to iterate through each bit in a 32 bit representation
+        while(n != 0 && position >= 0){
+            int a = n & 1;
+            a <<= position;
+            reversed = reversed | a;
+            position--;
+            n >>= 1;
+        }
+        return reversed;
     }
     public static void main(String... args){
         int x = 10;
---
+++ leetcode/RotateArray.java
@@ -0,0 +1,15 @@
+// Given an array, rotate the array to the right by k steps, where k is non-negative.
+class RotateArray {
+    // Time: O(n) Space: O(n)
+    public void rotate(int[] nums, int k) {
+        int[] result = new int[nums.length];
+        for(int i = 0; i < nums.length; i++){
+            int cpy = nums[i];
+            int rot = (i+k)%nums.length;        // (current iteration + k skips) % arr.length = new location for arr[i]
+            result[rot] = cpy;
+        }
+        for(int i = 0; i < nums.length; i++){
+            nums[i] = result[i];
+        }
+    }
+}
