cc9d39f8e1892c9854f22cbc5bc0b8764bb02bf7
Solviento <perej8169@gmail.com>
2019-02-20 21:31:31 -0500

    updates

--- leetcode/SingleNumber.java
+++ leetcode/SingleNumber.java
@@ -2,15 +2,21 @@
 Note:
 Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory? */
 class SingleNumber {
-  public int singleNumber(int[] nums) {
-    // a XOR 0 = a
+  static int singleNumber(int[] nums) {
+    // a XOR 0 = a    DO THE BIT MATH CALCULATIONS BY HAND TO VERIFY
     // a XOR a = 0
     // a XOR b XOR a = a XOR a XOR b = 0 XOR b = b
     int rem = 0;
     for (Integer e : nums) {
-      rem = rem ^ e;
+      rem = rem ^ e;  // ALSO DO THESE BIT CALCULATIONS TO SEE WHY XOR WORKS AND & + | DONT
     }
     return rem;
   }
   // Input:[4,1,2,1,2] Output:4
+  public static void main(String... args){
+    // int[] nums = new int[]{1,1,2,3,3};
+    int[] nums = new int[] {1,2};
+    int uni = singleNumber(nums);
+    System.out.println(uni); 
+  }
 }
---
+++ leetcode/SumNumbers.java
@@ -0,0 +1,43 @@
+class SumNumbers{
+  static int sumNumbers(String str) {
+    int sum = 0;
+    String numStr = "";
+    boolean currNum = false;
+    for (int i = 0; i < str.length(); i++) {
+      char c = str.charAt(i);
+      if(Character.isDigit(c)){
+        // System.out.println(c);
+        if(currNum == false){
+          currNum = true;
+          numStr += String.valueOf(c);
+          continue;
+        }
+        else{
+          numStr += String.valueOf(c);
+          continue;
+        }
+      }
+      if(!Character.isDigit(c)){
+        if(currNum == true){
+          sum += Integer.valueOf(numStr);
+          // System.out.println(numStr);
+          numStr = "";
+          currNum = false;
+          continue;
+        }
+        else{
+          continue;
+        }
+      }
+    }
+    // any left over numStr values are added, ie. when a number is at the end of the string: "abc123z11"
+    if(numStr.length() > 0)
+      sum += Integer.valueOf(numStr);
+    return sum;
+  }
+  public static void main(String... args){
+    String s = "aa11b33";
+    int sum = sumNumbers(s);
+    System.out.println("Sum: " + sum);
+  }
+}
