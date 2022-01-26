50c1181906a7d4cd214586928796539c58834d81
Solviento <perej8169@gmail.com>
2022-01-26 11:50:06 -0500

    Code

--- interview-prep/python-questions/src/main.py
+++
@@ -1,8 +0,0 @@
-# default main py file
-# project contains all python examples for coding interviews
-
-def print_hi(name):
-    print('Hi,', name)
-
-if __name__ == '__main__':
-    print_hi('Jason')
