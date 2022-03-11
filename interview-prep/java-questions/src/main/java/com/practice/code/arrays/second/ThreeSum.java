95213696d2ac222e66d55248933a3986366abbce
Solviento <perej8169@gmail.com>
2022-03-11 11:26:03 -0500

    Code

--- interview-prep/java-questions/src/main/java/com/practice/code/arrays/second/ThreeSum.java
+++ interview-prep/java-questions/src/main/java/com/practice/code/arrays/second/ThreeSum.java
@@ -1,9 +1,10 @@
-/*Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+/*Given an integer array nums, return all the
+triplets [nums[i], nums[j], nums[k]] such
+that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
 
 Notice that the solution set must not contain duplicate triplets.
 
 Example 1:
-
 Input: nums = [-1,0,1,2,-1,-4]
 Output: [[-1,-1,2],[-1,0,1]]*/
 
@@ -21,11 +22,11 @@ public class ThreeSum implements CodeRunner {
         Arrays.sort(nums);
         List<List<Integer>> output_arr = new LinkedList<>();
         for (int i = 0; i < nums.length-2; i++) {
-            if (i == 0 || (i > 0 && nums[i] != nums[i-1])) {
+            if (i == 0 || nums[i] != nums[i - 1]) {
                 // sorted list, pick on upper and lower boundaries for match
                 int low = i + 1;
                 int high = nums.length - 1;
-                int sum = 0 - nums[i];
+                int sum = -nums[i];
                 while(low < high) {
                     if (nums[low] + nums[high] == sum) {
                         output_arr.add(Arrays.asList(nums[i], nums[low], nums[high]));
